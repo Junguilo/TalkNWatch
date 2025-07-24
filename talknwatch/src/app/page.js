2899727edd7d8908f5f21488e8ab472b4c877bc9
June Eguilos <tulcusss@gmail.com>
2025-07-23 23:55:23 -0700

    Save pallette for later

--- talknwatch/src/app/page.js
+++ talknwatch/src/app/page.js
@@ -2,6 +2,9 @@
 import HostRoom from './components/hostRoom'
 import styles from './page.module.css'
 
+/*
+https://coolors.co/palette/031926-468189-77aca2-9dbebb-f4e9cd
+*/
 export default function Home() {
   return (
     <div className={styles.pageBody}>
